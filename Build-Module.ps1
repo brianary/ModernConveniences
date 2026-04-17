207c5e8e9b4bc9c8b2e37f52e8e3fc3a937ab234
Brian Lalonde <brianary@gmail.com>
2026-04-17 15:41:09 -0700

    🚚 Reorganize a bit

--- Build-Module.ps1
+++ Build-Module.ps1
@@ -3,10 +3,8 @@
 Assembles the module file.
 #>
 
-#Requires -Version 7
-[CmdletBinding()] Param(
-[switch] $NextVersion
-)
+#Requires -Version 7.3
+[CmdletBinding()] Param()
 Begin
 {
     filter Format-Function
@@ -29,46 +27,21 @@ $(Get-Content $FullName -Raw)
     {
         [CmdletBinding()] Param()
         $Local:OFS = [Environment]::NewLine
-        $public = Get-Item src/public/*.ps1
+        $public = Get-Item public/*.ps1
         return @"
-$(Get-Item src/private/*.ps1 |Format-Function)
+$(Get-Item private/*.ps1 |Format-Function)
 $($public |Format-Function)
 Export-ModuleMember -Function $($public.BaseName -join ',')
 "@ |Out-File ([io.path]::ChangeExtension($(git rev-parse --show-toplevel |Split-Path -Leaf),'psm1')) utf8BOM
     }
 
-	function Get-NextModuleVersion
-	{
-		[CmdletBinding()] Param()
-		$psd1 = Import-PowerShellDataFile *.psd1
-		[version] $v = $psd1.ModuleVersion
-		return [version]::new($v.Major,$v.Minor,$v.Build,$v.Revision+1)
-	}
-
-	function Step-ModuleVersion
-	{
-		[CmdletBinding()] Param()
-		$psd1 = Get-Item *.psd1 |Select-Object -ExpandProperty Name -First 1
-		$version = Get-NextModuleVersion
-		@(Get-Content $psd1) |
-			ForEach-Object {
-				if($_ -notmatch '^(\s*ModuleVersion\s*=\s*'')(\d+(\.\d+){1,3})(''.*)$') {$_}
-				else {"$($Matches[1])$version$($Matches[4])"}
-			} |Out-File $psd1 utf8BOM
-		Write-Information "Updated $psd1 to v$version"
-	}
-
+	Push-Location "$PSScriptRoot/src"
 }
 Process
 {
-    try
-    {
-        Push-Location $PSScriptRoot
-        Out-Module
-		if($NextVersion) {Step-ModuleVersion}
-    }
-    finally
-    {
-        Pop-Location
-    }
+	Out-Module
+}
+Clean
+{
+	Pop-Location
 }

3b35e2fa9a9f8f8d9b92a3f076c63bd332d63813
Brian Lalonde <brianary@gmail.com>
2026-05-05 20:53:43 -0700

    ✨ Add encoding detection

---
+++ src/private/Read-FileEncoding.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+Does simple detection of a file's text encoding.
+#>
+
+[CmdletBinding()] Param(
+[Parameter(Position=0,Mandatory=$true)][string] $Path
+)
+if('EFBBBF' -eq ('{0:X2}{1:X2}{2:X2}' -f  (Get-Content ./README.md -AsByteStream -TotalCount 3))) {return 'utf8BOM'}
+$sr = New-Object System.IO.StreamReader $Path,$true
+$sr.Peek() |Out-Null
+switch($sr.CurrentEncoding.WebName)
+{
+	'utf-8'    {return 'utf8NoBOM'} # will also match Windows-1252, iso-8859-1, iso-8859-15
+	'utf-16'   {return 'unicode'}
+	'utf-16BE' {return 'bigendianunicode'}
+	'utf-32'   {return 'utf32'}
+	'utf-32BE' {return 'bigendianutf32'}
+	default    {return 'utf8BOM'}
+}

3ffb03c2ee23ec3b3800dbd57b32da2c09b63e53
Brian Lalonde <brianary@gmail.com>
2026-05-05 20:53:43 -0700

    ﻿✨ Add encoding detection

--- src/private/Read-FileEncoding.ps1
+++ src/private/Read-FileEncoding.ps1
@@ -3,10 +3,10 @@
 Does simple detection of a file's text encoding.
 #>
 
-[CmdletBinding()][OutputType([string])] Param(
+[CmdletBinding()] Param(
 [Parameter(Position=0,Mandatory=$true)][string] $Path
 )
-if('EFBBBF' -eq ('{0:X2}{1:X2}{2:X2}' -f  (Get-Content ./README.md -AsByteStream -TotalCount 3))) {return 'utf8BOM'}
+if('EFBBBF' -eq ('{0:X2}{1:X2}{2:X2}' -f  (Get-Content $Path -AsByteStream -TotalCount 3))) {return 'utf8BOM'}
 $sr = New-Object System.IO.StreamReader $Path,$true
 $sr.Peek() |Out-Null
 switch($sr.CurrentEncoding.WebName)

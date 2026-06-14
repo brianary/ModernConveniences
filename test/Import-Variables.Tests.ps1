833a7dcaf41dead40ce20452a9d44a7d9a7c5937
Brian Lalonde <brianary@gmail.com>
2026-06-14 06:27:29 -0700

    TRying to fix scope math

--- test/Import-Variables.Tests.ps1
+++ test/Import-Variables.Tests.ps1
@@ -3,8 +3,6 @@
 Tests creating local variables from a data row or dictionary (hashtable).
 #>
 
-#TODO: Fix or test without Pester. Pester seems incapable of testing this.
-return
 if(!(&"$PSScriptRoot/../scripts/Test-RelevantTest.ps1")) {return}
 BeforeAll {
 	Set-StrictMode -Version Latest
@@ -12,21 +10,48 @@ BeforeAll {
 }
 Describe 'Import-Variables' -Tag Import-Variables,Import,Variables {
 	Context 'Adds variables from object properties' {
-		It "The properties of '<InputObject>' are added as variables using parameter call" -TestCases @(
-			@{ InputObject = [pscustomobject]@{ Id = 1; Name = 'Something' } }
+		$guid = (New-Guid).ToString('N')
+		It "should add properties of '<InputObject>' as variables using parameter call" -TestCases @(
+			@{ InputObject = [pscustomobject]@{ "Id$guid" = 1; "Name$guid" = 'Something' } }
 		) {
 			Param([psobject] $InputObject)
 			Import-Variables $InputObject
 			$InputObject.PSObject.Properties |
 				ForEach-Object {
-					Get-Variable $_.Name -ErrorAction Ignore |
+					Get-Variable $_.Name -ErrorAction Ignore -OutVariable variable |
 						Should -BeTrue -Because 'variable should exist'
-					Get-Variable $_.Name -ValueOnly |
+					$i = 0
+					foreach($parent in Get-PSCallStack |Select-Object -Skip 1)
+					{
+						$where = "$($parent.Command) in $($parent.ScriptName):$($parent.ScriptLineNumber)"
+						Get-Variable $_.Name -Scope (++$i) -ErrorAction Ignore |
+							Should -Not -BeTrue -Because "'$($_.Name)' variable overshot local scope to $i, $where"
+					}
+					Get-Variable $_.Name -Scope Local -ErrorAction Ignore |
+						Should -BeTrue -Because 'variable should exist in local scope'
+					Get-Variable $_.Name -ValueOnly -Scope Local |
 						Should -BeExactly $_.Value -Because 'variable value should match'
 				}
 		}
-		It "The properties of '<InputObject>' are added as variables using pipeline" -TestCases @(
-			@{ InputObject = [pscustomobject]@{ Id = 1; Name = 'Something' } }
+		$guid = (New-Guid).ToString('N')
+		It "should add keys of '<InputObject>' as variables using parameter call" -Skip -TestCases @(
+			@{ InputObject = @{ "Id$guid" = 1; "Name$guid" = 'Something' } }
+		) {
+			Param([psobject] $InputObject)
+			Import-Variables $InputObject
+			$InputObject.GetEnumerator() |
+				ForEach-Object {
+					Get-Variable $_.Name -ErrorAction Ignore |
+						Should -BeTrue -Because 'variable should exist'
+					Get-Variable $_.Name -Scope Local -ErrorAction Ignore |
+						Should -BeTrue -Because 'variable should exist in local scope'
+					Get-Variable $_.Name -ValueOnly -Scope Local |
+						Should -BeExactly $_.Value -Because 'variable value should match'
+				}
+		}
+		$guid = (New-Guid).ToString('N')
+		It "should add properties of '<InputObject>' as variables using pipeline" -Skip -TestCases @(
+			@{ InputObject = [pscustomobject]@{ "Id$guid" = 1; "Name$guid" = 'Something' } }
 		) {
 			Param([psobject] $InputObject)
 			$InputObject |Import-Variables
@@ -34,7 +59,25 @@ Describe 'Import-Variables' -Tag Import-Variables,Import,Variables {
 				ForEach-Object {
 					Get-Variable $_.Name -ErrorAction Ignore |
 						Should -BeTrue -Because 'variable should exist'
-					Get-Variable $_.Name -ValueOnly |
+					Get-Variable $_.Name -Scope Local -ErrorAction Ignore |
+						Should -BeTrue -Because 'variable should exist in local scope'
+					Get-Variable $_.Name -ValueOnly -Scope Local |
+						Should -BeExactly $_.Value -Because 'variable value should match'
+				}
+		}
+		$guid = (New-Guid).ToString('N')
+		It "should add properties of '<InputObject>' as variables using pipeline" -Skip -TestCases @(
+			@{ InputObject = @{ "Id$guid" = 1; "Name$guid" = 'Something' } }
+		) {
+			Param([psobject] $InputObject)
+			$InputObject |Import-Variables
+			$InputObject.GetEnumerator() |
+				ForEach-Object {
+					Get-Variable $_.Name -ErrorAction Ignore |
+						Should -BeTrue -Because 'variable should exist'
+					Get-Variable $_.Name -Scope Local -ErrorAction Ignore |
+						Should -BeTrue -Because 'variable should exist in local scope'
+					Get-Variable $_.Name -ValueOnly -Scope Local |
 						Should -BeExactly $_.Value -Because 'variable value should match'
 				}
 		}

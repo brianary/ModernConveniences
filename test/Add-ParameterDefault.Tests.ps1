d63f0c7cc49541d9c48bef05d7a5f57aa4d2303d
Brian Lalonde <brianary@gmail.com>
2026-07-04 20:36:01 -0700

    🐛 Fix more caller scope variables

--- test/Add-ParameterDefault.Tests.ps1
+++ test/Add-ParameterDefault.Tests.ps1
@@ -3,7 +3,6 @@
 Tests appending or creating a value to use for the specified cmdlet parameter to use when one is not specified.
 #>
 
-return #TODO
 if(!(&"$PSScriptRoot/../scripts/Test-RelevantTest.ps1")) {return}
 BeforeAll {
 	Set-StrictMode -Version Latest
@@ -12,19 +11,28 @@ BeforeAll {
 Describe 'Add-ParameterDefault' -Tag Add-ParameterDefault,Add,ParameterDefault {
 	Context 'Appends or creates a value to use for the specified cmdlet parameter to use when one is not specified.' {
 		It "Should set a simple default" {
-			$initialCount = $PSDefaultParameterValues.Count
-			Add-ParameterDefault gcm Type All -Scope Script
-			$PSDefaultParameterValues.Count |Should -BeGreaterThan $initialCount `
-				-Because 'the number of defaults should increase after adding one'
-			$PSDefaultParameterValues.ContainsKey('Get-Command:CommandType') |
-				Should -BeTrue -Because 'defaults should be added after looking up cmdlet and param aliases'
-			$PSDefaultParameterValues['Get-Command:CommandType'] |Should -BeExactly All
+			function Trace-GcmDefault
+			{
+				[CmdletBinding()] Param()
+				Add-ParameterDefault gcm Type All
+				$PSDefaultParameterValues.ContainsKey('Get-Command:CommandType') |
+					Should -BeTrue -Because 'defaults should be added after looking up cmdlet and param aliases'
+				$PSDefaultParameterValues['Get-Command:CommandType'] |Should -BeExactly All
+				Remove-Variable PSDefaultParameterValues -Scope Local -ErrorAction Ignore
+			}
+			Trace-GcmDefault
 		}
 		It "Should set a hashtable default" {
-			Add-ParameterDefault Select-Xml Namespace @{svg = 'http://www.w3.org/2000/svg'}
-			$PSDefaultParameterValues.ContainsKey('Select-Xml:Namespace') |Should -BeTrue
-			$PSDefaultParameterValues['Select-Xml:Namespace'].ContainsKey('svg') |Should -BeTrue
-			$PSDefaultParameterValues['Select-Xml:Namespace']['svg'] |Should -BeExactly 'http://www.w3.org/2000/svg'
+			function Add-HashtableDefault
+			{
+				[CmdletBinding()] Param()
+				Add-ParameterDefault Select-Xml Namespace @{'___' = 'urn:ietf:rfc:2648'}
+				$PSDefaultParameterValues.ContainsKey('Select-Xml:Namespace') |Should -BeTrue
+				$PSDefaultParameterValues['Select-Xml:Namespace'].ContainsKey('___') |Should -BeTrue
+				$PSDefaultParameterValues['Select-Xml:Namespace']['___'] |Should -BeExactly 'urn:ietf:rfc:2648'
+				Remove-Variable PSDefaultParameterValues -Scope Local -ErrorAction Ignore
+			}
+			Add-HashtableDefault
 		}
 	}
 }
